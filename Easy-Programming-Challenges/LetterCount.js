987239239b7a6d6c3aafe546c091971380089080
Veronika Benkeser <veronika.benkeser@gmail.com>
2015-04-10 15:48:42 -0700

    Create LetterCount.js

---
+++ Easy-Programming-Challenges/LetterCount.js
@@ -0,0 +1,23 @@
+function LetterCountI(str) { 
+str = str.toLowerCase();
+var array = str.split(" ");
+var max = 0; 
+var result =-1;
+
+for(var i in array){
+
+   var res = array[i].toString().split("").sort().join("").toString();
+   
+   if(/(.)\1/g.test(res)){
+       
+       console.log("cat");
+    var l = res.match(/(.)\1/g).toString().length;
+    console.log(l);
+    if (l >max){
+        max=l;
+        result = array[i];
+    }
+}
+}
+return result;
+}

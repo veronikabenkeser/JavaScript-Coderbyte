9bdaa34d369e7b94345d0684002556f840b05a52
Veronika Benkeser <veronika.benkeser@gmail.com>
2015-04-10 16:08:52 -0700

    Update LetterCount.js

--- Easy-Programming-Challenges/LetterCount.js
+++ Easy-Programming-Challenges/LetterCount.js
@@ -5,19 +5,16 @@ var max = 0;
 var result =-1;
 
 for(var i in array){
-
    var res = array[i].toString().split("").sort().join("").toString();
    
    if(/(.)\1/g.test(res)){
-       
-       console.log("cat");
-    var l = res.match(/(.)\1/g).toString().length;
-    console.log(l);
-    if (l >max){
-        max=l;
-        result = array[i];
-    }
-}
+       var l = res.match(/(.)\1/g).toString().length;
+       console.log(l);
+       if (l >max){
+           max=l;
+           result = array[i];
+       }
+   }
 }
 return result;
 }
